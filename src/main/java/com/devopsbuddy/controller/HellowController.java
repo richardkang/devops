4c9129af50aeb0a1c00a2fb23fc8b7505611959b
richardkang <richardkang.tw@yahoo.com.tw>
2017-10-27 11:15:07 +0800

    no message

---
+++ src/main/java/com/devopsbuddy/controller/HellowController.java
@@ -0,0 +1,14 @@
+package com.devopsbuddy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HellowController {
+	
+	@RequestMapping("/")
+	public String sayHello() {
+		return "index";
+	}
+
+}
